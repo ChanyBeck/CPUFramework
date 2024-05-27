18a9caa682e8a63ba2e4ef196bd2ac7fc8d54d81
Chanybeck <chanybeck01@gmail.com>
2024-05-27 15:16:03 -0400

    Recipe Sprocs

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -10,22 +10,114 @@ namespace CPUFramework
     {
 
         public static string ConnectionString = "";
-        public static DataTable ExecuteSQL(string sqlstatement)
+
+        public static SqlCommand GetSQLCommand(string sprocname)
+        {
+            SqlCommand cmd;
+            using (SqlConnection conn = new SqlConnection(SQLUtility.ConnectionString))
+            {
+                cmd = new SqlCommand(sprocname, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                conn.Open();
+                SqlCommandBuilder.DeriveParameters(cmd);
+            }
+            return cmd;
+        }
+
+        public static DataTable GetDataTable(SqlCommand cmd) {
+            return DoExecuteSQL(cmd, true);
+        }
+
+        public static void SaveDataRow(DataRow row, string sprocname)
+        {
+            SqlCommand cmd = GetSQLCommand(sprocname);
+            foreach(DataColumn col in row.Table.Columns)
+            {
+                string paramname = $"@{col.ColumnName}";
+                if (cmd.Parameters.Contains(paramname) == true)
+                {
+                    cmd.Parameters[paramname].Value = row[col.ColumnName];
+                }
+            }
+            DoExecuteSQL(cmd, false);
+            foreach(SqlParameter p in cmd.Parameters)
+            {
+                if(p.Direction == ParameterDirection.InputOutput)
+                {
+                    string colname = p.ParameterName.Substring(1);
+                    if (row.Table.Columns.Contains(colname))
+                    {
+                        row[colname] = p.Value;
+                    }
+                }
+            }
+        }
+        private static DataTable DoExecuteSQL(SqlCommand cmd, bool loadtable)
         {
             DataTable dt = new();
-            SqlConnection conn = new();
-            conn.ConnectionString = ConnectionString;
-            conn.Open();
-
-            SqlCommand cmd = new();
-            cmd.Connection = conn;
-            cmd.CommandText = sqlstatement;
-            SqlDataReader dr = cmd.ExecuteReader();
-            dt.Load(dr);
-
+            using (SqlConnection conn = new SqlConnection(SQLUtility.ConnectionString))
+            {
+                conn.Open();
+                cmd.Connection = conn;
+                try
+                {
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    CheckReturnValue(cmd);
+                    if (loadtable == true)
+                    {
+                        dt.Load(dr);
+                    }
+                }
+                catch(SqlException ex)
+                {
+                    string msg = ParseConstraintMessage(ex.Message);
+                    throw new Exception(msg);
+                }
+            }
             AllowNulls(dt);
-
             return dt;
+        }
+
+        private static void CheckReturnValue(SqlCommand cmd)
+        {
+            int returnvalue = 0;
+            string msg = "";
+            if (cmd.CommandType == CommandType.StoredProcedure)
+            {
+                foreach (SqlParameter p in cmd.Parameters)
+                {
+                    if (p.Direction == ParameterDirection.ReturnValue)
+                    {
+                        if (p.Value != null)
+                        {
+                            returnvalue = (int)p.Value;
+                        }
+                    }
+                    else if (p.ParameterName.ToLower() == "@message")
+                        if (p.Value != null)
+                        {
+                            msg = p.Value.ToString();
+                        }
+                }
+                if (returnvalue == 1)
+                {
+                    if (msg == "")
+                    {
+                        msg = $"{cmd.CommandText} was not completed";
+                    }
+                    throw new Exception(msg);
+                }
+            }
+        }
+        public static DataTable ExecuteSQL(string sqlstatement)
+        {
+            Debug.Print(sqlstatement); 
+            return DoExecuteSQL(new SqlCommand(sqlstatement), true);
+        }
+        public static void ExecuteSQL(SqlCommand cmd)
+        {
+            DoExecuteSQL(cmd, false);
         }
 
         private static void AllowNulls(DataTable dt)
@@ -44,6 +136,55 @@ namespace CPUFramework
                     Debug.Print(c.ColumnName + " = " + r[c.ColumnName].ToString());
                 }
             }
+        }
+        public static void SetParameterValue(SqlCommand cmd, string paramnane, object value)
+        {
+            cmd.Parameters[paramnane].Value = value;
+        }
+        public static string ParseConstraintMessage(string msg)
+        {
+            string origmsg = msg;
+            string prefix = "ck_";
+            string msgend = "";
+            if(msg.Contains(prefix) == false)
+            {
+                if (msg.Contains("u_"))
+                {
+                    prefix = "u_";
+                    msgend = " must be unique";
+                }
+                else if (msg.Contains("f_"))
+                {
+                    prefix = "f_";
+                }
+            }
+            if (msg.Contains(prefix))
+            {
+                msg = msg.Replace("\"", "'");
+                int pos = msg.IndexOf(prefix) + prefix.Length;
+                msg = msg.Substring(pos);
+                pos = msg.IndexOf("\'");
+                if (pos == -1)
+                {
+                    msg = origmsg;
+                }
+                else
+                {
+                    msg = msg.Substring(0, pos);
+                    msg = msg.Replace("_", " ");
+                    msg = msg + msgend;
+
+                    if(prefix == "f_")
+                    {
+                        var words = msg.Split(" ");
+                        if (words.Length > 0)
+                        {
+                            msg = $"Cannot delete {words[0]} bcause it has a related {words[1]} record";
+                        }
+                    }
+                }
+            }
+            return msg;
         }
         public static int GetFirstColumnFirstRowValue(string sql)
         {
